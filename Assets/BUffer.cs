ec1457b87e338b00af4d1fdc7e4d1f25ac522d58
SeyedMortezaKamali <smkplus3d@gmail.com>
2019-04-06 09:25:52 +0430

    Add Particle

---
+++ Assets/BUffer.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BUffer : MonoBehaviour {
+	public RenderTexture RT;
+	public Material mat;
+
+private void OnRenderImage(RenderTexture src, RenderTexture dest) {
+	Graphics.Blit(src,dest,mat);
+}
+}
